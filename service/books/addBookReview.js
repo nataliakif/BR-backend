3c188dde7ed2cd0ca50026560edd7f370e0d4fda
Oleh Subotin <subotinoleg0@gmail.com>
2022-09-26 00:50:31 +0300

    bug fix

--- service/books/addBookReview.js
+++ service/books/addBookReview.js
@@ -1,8 +1,8 @@
 const { Book } = require("../../models/book");
 
-const addBookReview = async (id, body) => {
+const addBookReview = async (id, rating, review) => {
     try {
-        const data = await Book.findByIdAndUpdate(id, { ...body }, { new: true });
+        const data = await Book.findByIdAndUpdate(id, { rating, review }, { new: true });
         return data;
     } catch (error) {
         console.log(error.message);
