debd6423f05d6605b430672bf0c81c4ffee11f20
Oleh Subotin <subotinoleg0@gmail.com>
2022-09-27 00:11:39 +0300

    add authentication

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const { books: ctrl } = require('../../controllers');
-const { validation, ctrlWrapper } = require("../../middlewares");
+const { validation, ctrlWrapper, authenticate } = require("../../middlewares");
 const { joiSchema, joiBookReview } = require("../../models/book");
 
 const router = express.Router()
 
-router.post('/', validation(joiSchema), ctrlWrapper(ctrl.addBook));
+router.post('/', authenticate, validation(joiSchema), ctrlWrapper(ctrl.addBook));
 
-router.post('/review', validation(joiBookReview), ctrlWrapper(ctrl.addBookReview));
+router.post('/review', authenticate, validation(joiBookReview), ctrlWrapper(ctrl.addBookReview));
 
 
 module.exports = router;
