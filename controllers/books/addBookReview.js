8cacd0163793af513b7959da6156f03b3bfc244c
Oleh Subotin <subotinoleg0@gmail.com>
2022-09-26 23:56:39 +0300

    bug fix

--- controllers/books/addBookReview.js
+++ controllers/books/addBookReview.js
@@ -2,6 +2,9 @@ const { book: service } = require("../../service");
 
 const addBookReview = async (req, res) => {
     const { _id, rating, review } = req.body;
+    if (!_id) {
+        throw new Error("user not authorized")
+    }
     const result = await service.addBookReview(_id, rating, review);
     res.status(201).json({
         status: 'success',
