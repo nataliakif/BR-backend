67a55636c1365d8d8125319775b0282d1e7e88ea
Oleh Subotin <subotinoleg0@gmail.com>
2022-09-30 17:03:48 +0300

    get planning by owner

--- controllers/planning/index.js
+++ controllers/planning/index.js
@@ -2,10 +2,12 @@ const startPlanning = require('./startPlanning');
 const getPlanning = require('./getPlanning');
 const updatePlanningPages = require('./updatePlanningPages');
 const deletePlanning = require('./deletePlanning');
+const getPlanningByOwner = require('./getPlanningByOwner');
 
 module.exports = {
     startPlanning,
     getPlanning,
     updatePlanningPages,
-    deletePlanning
+    deletePlanning,
+    getPlanningByOwner
 }
--- service/planning/index.js
+++ service/planning/index.js
@@ -2,10 +2,12 @@ const startPlanning = require('./startPlanning');
 const getPlanningById = require('./getPlanningById');
 const updatePlanningPages = require('./updatePlanningPages');
 const deletePlanning = require('./deletePlanning');
+const getPlanningByOwner = require('./getPlanningByOwner');
 
 module.exports = {
     startPlanning,
     getPlanningById,
     updatePlanningPages,
-    deletePlanning
+    deletePlanning,
+    getPlanningByOwner
 }
