fad40f57563d29fed58283e8078956a052d228ce
@s_zhsn <s.zhosan@gmail.com>
2022-10-02 18:27:29 +0300

    fixed

--- controllers/books/addBookReview.js
+++ controllers/books/addBookReview.js
@@ -1,21 +1,22 @@
-const { NotFound } = require('http-errors');
+const { NotFound } = require("http-errors");
 const { book: service } = require("../../service");
 
 const addBookReview = async (req, res) => {
-    const { bookId } = req.params;
-    const newBook =  req.body;
-    const book = await service.getBookByBookId(bookId);
-    if (!book) {
-        throw new NotFound(404, "There is no book with such id.");
-    };
-    const result = await service.addBookReview(bookId, newBook);
-    res.status(200).json({
-        status: 'success',
-        code: 200,
-        data: {
-            result
-        }
-    });
+  const { bookId } = req.params;
+  const newBook = req.body;
+  console.log(req.body);
+  const book = await service.getBookByBookId(bookId);
+  if (!book) {
+    throw new NotFound(404, "There is no book with such id.");
+  }
+  const result = await service.addBookReview(bookId, newBook);
+  res.status(200).json({
+    status: "success",
+    code: 200,
+    data: {
+      result,
+    },
+  });
 };
 
 module.exports = addBookReview;
